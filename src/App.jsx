dc9b11caa5cfb9d85e8a41ff980cf3b5897ffe45
AjaySingh <ajayingh03@gmail.com>
2024-09-27 22:22:29 +0530

    added home page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Header from '../Components/Header/header';
+import Hero from '../Components/hero/hero';
+import Card from '../Components/Card/card';
+import Grid from '../Components/Grid/grid,';
+import Footer from '../Components/Footer/footer';
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Hero />
+      <Card/>
+      <Grid/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
